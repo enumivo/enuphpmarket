0eedc32007c3df8ccc5ac617cdef70cd83240893
enumivo <admin@enumivo.org>
2018-10-13 22:25:18 +0800

    amount/enu_balance

--- ex.cpp
+++ ex.cpp
@@ -31,7 +31,7 @@ void ex::buyPHP(const currency::transfer &transfer) {
   auto fee = php_buy / 500;
   auto php_transfer_amount = php_buy - fee;
 
-  auto test = pow(1+amount/enu_balance,0.05);
+  auto test = amount/enu_balance;
 
   auto to = transfer.from;
 
