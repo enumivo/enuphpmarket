ebab507193792f4862db9a5317350f5ae6246b1b
enumivo <admin@enumivo.org>
2018-10-13 22:28:07 +0800

    percentage added

--- ex.cpp
+++ ex.cpp
@@ -31,7 +31,7 @@ void ex::buyPHP(const currency::transfer &transfer) {
   auto fee = php_buy / 500;
   auto php_transfer_amount = php_buy - fee;
 
-  auto test = amount/enu_balance;
+  auto test = 100*amount/enu_balance;
 
   auto to = transfer.from;
 
