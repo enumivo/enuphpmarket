52f3d9b2eaf44b7de5a2b73cf009ee1279526ca9
enumivo <admin@enumivo.org>
2018-10-14 00:09:22 +0800

    .

--- ex.cpp
+++ ex.cpp
@@ -34,7 +34,7 @@ void ex::buyPHP(const currency::transfer &transfer) {
   double amt = amount;
   double bal = enu_balance;
   double res = php_balance/10000;
-  double tobuy = res*(pow(1+amt/bal,0.5)-1);
+  double tobuy = pow(1+amt/bal,0.5)-1;
 
 
   auto to = transfer.from;
@@ -43,7 +43,7 @@ void ex::buyPHP(const currency::transfer &transfer) {
 
   action(permission_level{_self, N(active)}, N(coin), N(transfer),
          std::make_tuple(_self, to, quantity,
-                         std::string("Buy PHP with ENU ")+std::to_string(tobuy)))
+                         std::string("Buy PHP with ENU ")+std::to_string(res)+' '+std::to_string(tobuy)))
       .send();
 }
 
